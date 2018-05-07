40b965cb29c5aa95845c0b44fe345fef1eea279b
Sam Popowich <spopowich@gmail.com>
2018-05-07 10:54:48 -0600

    Fix bugs.

--- app/helpers/blacklight/facets_helper_behavior.rb
+++ app/helpers/blacklight/facets_helper_behavior.rb
@@ -161,12 +161,24 @@ module Blacklight::FacetsHelperBehavior
   def render_selected_facet_value(facet_field, item)
     remove_href = search_action_path(search_state.remove_facet_params(facet_field, item))
     content_tag(:span, class: "facet-label") do
+      if @locations[item.value]
+
+        content_tag(:span, facet_display_value(facet_field, @locations[item.value]["name"]), class: "selected") +
+        # remove link
+        link_to(remove_href, class: "remove") do
+        content_tag(:span, '', class: "glyphicon glyphicon-remove") +
+          content_tag(:span, '[remove]', class: 'sr-only')
+      end
+
+     else
+
       content_tag(:span, facet_display_value(facet_field, item), class: "selected") +
         # remove link
         link_to(remove_href, class: "remove") do
         content_tag(:span, '', class: "glyphicon glyphicon-remove") +
           content_tag(:span, '[remove]', class: 'sr-only')
       end
+    end
     end + render_facet_count(item.hits, :classes => ["selected"])
   end
 
